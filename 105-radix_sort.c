2c8c833cb314826787a3cbdb21acc21a0239acbc
Emmanuel Makokha <99004575+MamaiTheCoder@users.noreply.github.com>
2023-01-23 16:25:31 +0300

    Update 105-radix_sort.c

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,56 +1,35 @@
 #include "sort.h"
-
 /**
- * radix_sort - sorts an array following the Radix sort algorithm
- * @array: array of ints to sort
+ * radix_sort - sorts an array of integers in ascending
+ * order using the Radix sort algorithm
+ *
+ * @array: input array
  * @size: size of the array
  */
 void radix_sort(int *array, size_t size)
 {
-	int max;
-	size_t i, lsd;
+	int flag = 1, n = 10;
+	size_t i, f;
 
-	if (!array || size < 2)
+	if (!array || size == 1)
 		return;
-
-	max = 0;
-	for (i = 0; i < size; i++)
-		if (array[i] > max)
-			max = array[i];
-
-	for (lsd = 1; max / lsd > 0; lsd *= 10)
+	while (flag)
 	{
-		count_sort_LSD(array, size, lsd);
+		flag = 0;
+		for (i = 1, f = 1; i <  size; i++, f++)
+		{
+			if ((array[i - 1] % (n * 10)) / ((n * 10) / 10) > 0)
+				flag = 1;
+			if (((array[i - 1] % n) / (n / 10)) > ((array[i] % n) / (n / 10)))
+			{
+				array[i - 1] = array[i - 1] + array[i];
+				array[i] = array[i - 1] - array[i];
+				array[i - 1] = array[i - 1] - array[i];
+				if ((i - 1) > 0)
+					i = i - 2;
+			}
+		}
 		print_array(array, size);
+		n = n * 10;
 	}
-}
-
-/**
- * count_sort_LSD - count sort with LSD
- * @array: array to sort
- * @size: size of the array
- * @lsd: least significant digit
- */
-void count_sort_LSD(int *array, size_t size, size_t lsd)
-{
-	int count_arr[10] = {0}, *out_arr, l, m;
-	size_t k, n;
-
-	out_arr = malloc(sizeof(int) * size);
-
-	for (k = 0; k < size; k++)
-		count_arr[(array[k] / lsd) % 10]++;
-	for (l = 1; l < 10; l++)
-		count_arr[l] += count_arr[l - 1];
-
-	for (m = size - 1; m >= 0; m--)
-	{
-		out_arr[count_arr[(array[m] / lsd) % 10] - 1] = array[m];
-		count_arr[(array[m] / lsd) % 10]--;
-	}
-
-	for (n = 0; n < size; n++)
-		array[n] = out_arr[n];
-
-	free(out_arr);
 }
